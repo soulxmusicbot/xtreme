9aaae31ed020c628b94afca17d30e8db6bc77873
ɪɴᴠᴀʟɪᴅ <116873243+ifeelscam@users.noreply.github.com>
2025-01-09 11:22:17 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,7 +7,8 @@ from pyrogram.enums import ParseMode
 import sys
 from datetime import datetime
 
-from config import API_HASH, APP_ID, LOGGER, TG_BOT_TOKEN, TG_BOT_WORKERS, FORCE_SUB_CHANNEL, CHANNEL_ID, PORT
+from config import API_HASH, APP_ID, LOGGER, TG_BOT_TOKEN, TG_BOT_WORKERS, FORCE_SUB_CHANNEL, FORCE_SUB_CHANNEL2, CHANNEL_ID, LOG_CHANNEL, PORT
+
 
 name ="""
  CODEFLEX 
@@ -46,6 +47,19 @@ class Bot(Client):
                 self.LOGGER(__name__).warning(f"Please Double check the FORCE_SUB_CHANNEL value and Make sure Bot is Admin in channel with Invite Users via Link Permission, Current Force Sub Channel Value: {FORCE_SUB_CHANNEL}")
                 self.LOGGER(__name__).info("\nBot Stopped. https://t.me/offchats for support")
                 sys.exit()
+        if FORCE_SUB_CHANNEL2:
+            try:
+                link = (await self.get_chat(FORCE_SUB_CHANNEL2)).invite_link
+                if not link:
+                    await self.export_chat_invite_link(FORCE_SUB_CHANNEL2)
+                    link = (await self.get_chat(FORCE_SUB_CHANNEL2)).invite_link
+                self.invitelink2 = link
+            except Exception as a:
+                self.LOGGER(__name__).warning(a)
+                self.LOGGER(__name__).warning("Bot can't Export Invite link from Force Sub Channel!")
+                self.LOGGER(__name__).warning(f"Please Double check the FORCE_SUB_CHANNEL2 value and Make sure Bot is Admin in channel with Invite Users via Link Permission, Current Force Sub Channel Value: {FORCE_SUB_CHANNEL2}")
+                self.LOGGER(__name__).info("\nBot Stopped. https://t.me/offchats for support")
+                sys.exit()
         try:
             db_channel = await self.get_chat(CHANNEL_ID)
             self.db_channel = db_channel
@@ -60,28 +74,6 @@ class Bot(Client):
         self.set_parse_mode(ParseMode.HTML)
         self.username = usr_bot_me.username
         self.LOGGER(__name__).info(f"Bot Running..! Made by @OutlawBots")   
-     self.LOGGER(__name__).info(f""" \n\n       
-                                                   
- 
-░█████╗░██╗░░░██╗████████╗██╗░░░░░░█████╗░░██╗░░░░░░░██╗
-██╔══██╗██║░░░██║╚══██╔══╝██║░░░░░██╔══██╗░██║░░██╗░░██║
-██║░░██║██║░░░██║░░░██║░░░██║░░░░░███████║░╚██╗████╗██╔╝
-██║░░██║██║░░░██║░░░██║░░░██║░░░░░██╔══██║░░████╔═████║░
-╚█████╔╝╚██████╔╝░░░██║░░░███████╗██║░░██║░░╚██╔╝░╚██╔╝░
-░╚════╝░░╚═════╝░░░░╚═╝░░░╚══════╝╚═╝░░╚═╝░░░╚═╝░░░╚═╝░░
-██████╗░░█████╗░████████╗░██████╗
-██╔══██╗██╔══██╗╚══██╔══╝██╔════╝
-██████╦╝██║░░██║░░░██║░░░╚█████╗░
-██╔══██╗██║░░██║░░░██║░░░░╚═══██╗
-██████╦╝╚█████╔╝░░░██║░░░██████╔╝
-╚═════╝░░╚════╝░░░░╚═╝░░░╚═════╝░
-                                                         
- 
-                                                                        
-                                                                      
-                                                                                 
-                              
-                                          """)
 
         # Start Web Server
         app = web.AppRunner(await web_server())
@@ -89,7 +81,7 @@ class Bot(Client):
         await web.TCPSite(app, "0.0.0.0", PORT).start()
 
 
-        try: await self.send_message(OWNER_ID, text = f"<b><blockquote>🤖 Bᴏᴛ Rᴇsᴛᴀʀᴛᴇᴅ.....\nJoin @OutlawBots</blockquote></b>")
+        try: await self.send_message(OWNER_ID, text = f"<b><blockquote>🤖 Bᴏᴛ Rᴇsᴛᴀʀᴛᴇᴅ by @OutlawBots</blockquote></b>")
         except: pass
 
     async def stop(self, *args):
@@ -100,7 +92,7 @@ class Bot(Client):
         """Run the bot."""
         loop = asyncio.get_event_loop()
         loop.run_until_complete(self.start())
-        self.LOGGER(__name__).info("Bot is now running. Thanks to @faony")
+        self.LOGGER(__name__).info("Bot is now running. Thanks to @OutlawBots")
         try:
             loop.run_forever()
         except KeyboardInterrupt:
