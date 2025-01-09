6f8f4a931bd29efb685825f0afe484357398b849
ɪɴᴠᴀʟɪᴅ <116873243+ifeelscam@users.noreply.github.com>
2025-01-09 11:18:37 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -60,6 +60,28 @@ class Bot(Client):
         self.set_parse_mode(ParseMode.HTML)
         self.username = usr_bot_me.username
         self.LOGGER(__name__).info(f"Bot Running..! Made by @OutlawBots")   
+     self.LOGGER(__name__).info(f""" \n\n       
+                                                   
+ 
+░█████╗░██╗░░░██╗████████╗██╗░░░░░░█████╗░░██╗░░░░░░░██╗
+██╔══██╗██║░░░██║╚══██╔══╝██║░░░░░██╔══██╗░██║░░██╗░░██║
+██║░░██║██║░░░██║░░░██║░░░██║░░░░░███████║░╚██╗████╗██╔╝
+██║░░██║██║░░░██║░░░██║░░░██║░░░░░██╔══██║░░████╔═████║░
+╚█████╔╝╚██████╔╝░░░██║░░░███████╗██║░░██║░░╚██╔╝░╚██╔╝░
+░╚════╝░░╚═════╝░░░░╚═╝░░░╚══════╝╚═╝░░╚═╝░░░╚═╝░░░╚═╝░░
+██████╗░░█████╗░████████╗░██████╗
+██╔══██╗██╔══██╗╚══██╔══╝██╔════╝
+██████╦╝██║░░██║░░░██║░░░╚█████╗░
+██╔══██╗██║░░██║░░░██║░░░░╚═══██╗
+██████╦╝╚█████╔╝░░░██║░░░██████╔╝
+╚═════╝░░╚════╝░░░░╚═╝░░░╚═════╝░
+                                                         
+ 
+                                                                        
+                                                                      
+                                                                                 
+                              
+                                          """)
 
         # Start Web Server
         app = web.AppRunner(await web_server())
@@ -78,7 +100,7 @@ class Bot(Client):
         """Run the bot."""
         loop = asyncio.get_event_loop()
         loop.run_until_complete(self.start())
-        self.LOGGER(__name__).info("Bot is now running. Thanks to @rohit_1888")
+        self.LOGGER(__name__).info("Bot is now running. Thanks to @faony")
         try:
             loop.run_forever()
         except KeyboardInterrupt:
