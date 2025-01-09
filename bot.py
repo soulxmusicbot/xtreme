07b1eb2c31645b83c626ac3acc528e58597ea11f
ɪɴᴠᴀʟɪᴅ <116873243+ifeelscam@users.noreply.github.com>
2025-01-09 11:16:27 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,19 +1,16 @@
-#(©) @outlawbots  
-
 from aiohttp import web
 from plugins import web_server
-
+import asyncio
 import pyromod.listen
 from pyrogram import Client
 from pyrogram.enums import ParseMode
 import sys
 from datetime import datetime
 
-from config import API_HASH, APP_ID, LOGGER, TG_BOT_TOKEN, TG_BOT_WORKERS, FORCE_SUB_CHANNEL, FORCE_SUB_CHANNEL2, CHANNEL_ID, PORT
-
+from config import API_HASH, APP_ID, LOGGER, TG_BOT_TOKEN, TG_BOT_WORKERS, FORCE_SUB_CHANNEL, CHANNEL_ID, PORT
 
 name ="""
- BY INVALID  FROM TG
+ CODEFLEX 
 """
 
 
@@ -49,23 +46,10 @@ class Bot(Client):
                 self.LOGGER(__name__).warning(f"Please Double check the FORCE_SUB_CHANNEL value and Make sure Bot is Admin in channel with Invite Users via Link Permission, Current Force Sub Channel Value: {FORCE_SUB_CHANNEL}")
                 self.LOGGER(__name__).info("\nBot Stopped. https://t.me/offchats for support")
                 sys.exit()
-        if FORCE_SUB_CHANNEL2:
-            try:
-                link = (await self.get_chat(FORCE_SUB_CHANNEL2)).invite_link
-                if not link:
-                    await self.export_chat_invite_link(FORCE_SUB_CHANNEL2)
-                    link = (await self.get_chat(FORCE_SUB_CHANNEL2)).invite_link
-                self.invitelink2 = link
-            except Exception as a:
-                self.LOGGER(__name__).warning(a)
-                self.LOGGER(__name__).warning("Bot can't Export Invite link from Force Sub Channel!")
-                self.LOGGER(__name__).warning(f"Please Double check the FORCE_SUB_CHANNEL2 value and Make sure Bot is Admin in channel with Invite Users via Link Permission, Current Force Sub Channel Value: {FORCE_SUB_CHANNEL2}")
-                self.LOGGER(__name__).info("\nBot Stopped. https://t.me/offchats for support")
-                sys.exit()
         try:
             db_channel = await self.get_chat(CHANNEL_ID)
             self.db_channel = db_channel
-            test = await self.send_message(chat_id = db_channel.id, text = "Test Message")
+            test = await self.send_message(chat_id=db_channel.id, text="Test Message")
             await test.delete()
         except Exception as e:
             self.LOGGER(__name__).warning(e)
@@ -74,39 +58,30 @@ class Bot(Client):
             sys.exit()
 
         self.set_parse_mode(ParseMode.HTML)
-        self.LOGGER(__name__).info(f"Bot Running..!\n\nCreated by \nhttps://t.me/offchats")
-        self.LOGGER(__name__).info(f""" \n\n       
-                                                   
- 
-
-░█████╗░██╗░░░██╗████████╗██╗░░░░░░█████╗░░██╗░░░░░░░██╗
-██╔══██╗██║░░░██║╚══██╔══╝██║░░░░░██╔══██╗░██║░░██╗░░██║
-██║░░██║██║░░░██║░░░██║░░░██║░░░░░███████║░╚██╗████╗██╔╝
-██║░░██║██║░░░██║░░░██║░░░██║░░░░░██╔══██║░░████╔═████║░
-╚█████╔╝╚██████╔╝░░░██║░░░███████╗██║░░██║░░╚██╔╝░╚██╔╝░
-░╚════╝░░╚═════╝░░░░╚═╝░░░╚══════╝╚═╝░░╚═╝░░░╚═╝░░░╚═╝░░
-
-██████╗░░█████╗░████████╗░██████╗
-██╔══██╗██╔══██╗╚══██╔══╝██╔════╝
-██████╦╝██║░░██║░░░██║░░░╚█████╗░
-██╔══██╗██║░░██║░░░██║░░░░╚═══██╗
-██████╦╝╚█████╔╝░░░██║░░░██████╔╝
-╚═════╝░░╚════╝░░░░╚═╝░░░╚═════╝░
-                                                         
- 
-                                                                        
-                                                                      
-                                                                                 
-                              
-                                          """)
         self.username = usr_bot_me.username
-        #web-response
+        self.LOGGER(__name__).info(f"Bot Running..! Made by @OutlawBots")   
+
+        # Start Web Server
         app = web.AppRunner(await web_server())
         await app.setup()
-        bind_address = "0.0.0.0"
-        await web.TCPSite(app, bind_address, PORT).start()
+        await web.TCPSite(app, "0.0.0.0", PORT).start()
+
+
+        try: await self.send_message(OWNER_ID, text = f"<b><blockquote>🤖 Bᴏᴛ Rᴇsᴛᴀʀᴛᴇᴅ.....\nJoin @OutlawBots</blockquote></b>")
+        except: pass
 
     async def stop(self, *args):
         await super().stop()
         self.LOGGER(__name__).info("Bot stopped.")
-            
+
+    def run(self):
+        """Run the bot."""
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(self.start())
+        self.LOGGER(__name__).info("Bot is now running. Thanks to @rohit_1888")
+        try:
+            loop.run_forever()
+        except KeyboardInterrupt:
+            self.LOGGER(__name__).info("Shutting down...")
+        finally:
+            loop.run_until_complete(self.stop())
